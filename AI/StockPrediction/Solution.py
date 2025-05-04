7276d253e258d36eb601d982abc02c3701e3d37c
Rakesh Patel <rakesh.patel1978@gmail.com>
2025-05-04 13:55:24 +0530

    Updated comments to make it more clear

--- AI/StockPrediction/Solution.py
+++ AI/StockPrediction/Solution.py
@@ -5,10 +5,15 @@ def print_transactions(m, k, d, names, owned, prices):
         owned_shares = owned[i]
         price_series = prices[i]
 
-        recent = price_series[-3:]  # last 3 days
+        recent = price_series[-4:]  # last 4 days
+
+        # if price goes up, buy
         trend_up = all(x < y for x, y in zip(recent, recent[1:]))
+
+        # if price goes down, sell
         trend_down = all(x > y for x, y in zip(recent, recent[1:]))
 
+        #  the last number is the current stock price.
         current_price = price_series[-1]
 
         if trend_up:
@@ -25,23 +30,34 @@ def print_transactions(m, k, d, names, owned, prices):
 
 # Read input like main()
 if __name__ == "__main__":
-  targets = input().strip().split();
-  if len(targets) != 3:
-    sys.exit("Invalid input: expected 3 targets.")
-  
-  m = float(input())
-  k = int(input())
-  d = int(input())
+    targets = input().strip().split();
+    
+    if len(targets) != 3:
+        sys.exit("Invalid input: expected 3 targets.")
 
-  names = []
-  owned = []
-  prices = []
+    # m - the amount of money you could spend that day.
+    m = float(input())
 
-  for _ in range(k):
-    parts = input().strip().split()
-    names.append(parts[0])
-    owned.append(int(parts[1]))
-    price_list = list(map(float, parts[2:]))
-    prices.append(price_list)
+    # k - the number of different stocks available for buying or selling.
+    k = int(input())
 
-  print_transactions(m, k, d, names, owned, prices)
+    # d - the number of remaining days for trading stocks.
+    d = int(input())
+
+    #  the name of the stock (a string).
+    names = []
+
+    # the number of shares you own of that stock.
+    owned = []
+
+    # 5 space separated numbers representing the stock's price for the last 5 days.
+    prices = []
+
+    for _ in range(k):
+        parts = input().strip().split()
+        names.append(parts[0])
+        owned.append(int(parts[1]))
+        price_list = list(map(float, parts[2:]))
+        prices.append(price_list)
+
+    print_transactions(m, k, d, names, owned, prices)
